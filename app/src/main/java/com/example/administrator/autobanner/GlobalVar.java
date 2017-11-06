a6c76eeedea96d6fc4807c2e86725ae27b5ecfe8
Heyongwu <13366645979@163.com>
2017-11-06 10:30:11 +0800

    this is day01

---
+++ app/src/main/java/com/example/administrator/autobanner/GlobalVar.java
@@ -0,0 +1,5 @@
+package com.example.administrator.autobanner;
+
+public class GlobalVar {
+    public static int WIDTH;
+}
---
+++ app/src/main/java/com/example/administrator/autobanner/Webview.java
@@ -0,0 +1,14 @@
+package com.example.administrator.autobanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Webview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+    }
+}
